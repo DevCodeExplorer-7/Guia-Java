b96f159fa426032031a07ce31358f6a845ae1418
Pablo Burgos <111798287+DevCodeExplorer-7@users.noreply.github.com>
2023-11-02 20:24:44 -0300

    Ejercicio Maquina_Cafe

---
+++ Ejercicio6/Maquina_Cafe/src/main/java/org/example/Main.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import javax.swing.*;
+
+// Clase Main que contiene el menu de la maquina de cafe
+public class Main {
+    public static void main(String[] args) {
+        // Menu de la maquina de cafe
+        String menu = """
+                ***** Maquina de Cafe *****
+                1.- Cafe Express
+                2.- Total Venta ($)
+                3.- Cantidad de Cafe(vta)
+                4.- Salir
+                """;
+
+        // Instancia de la clase MaquinaCafe
+        MaquinaCafe maquinaCafe = new MaquinaCafe(1500,0,0);
+
+        int opcion, cantidaCafe, totalVenta;// Variables para el menu
+
+        // Ciclo do-while para el menu
+        do {
+            // Se muestra el menu y se pide la opcion
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+            switch (opcion) {
+                case 1: {
+                    // Se pide la cantidad de cafe y se calcula el total de la venta. se guarda en las variables de la clase MaquinaCafe
+                    JOptionPane.showMessageDialog(null, "Cafe Express");
+                    cantidaCafe = Integer.parseInt(JOptionPane.showInputDialog("Ingrese cantidad de cafe"));
+                    totalVenta = (maquinaCafe.getPrecio_cafe_express() * cantidaCafe);
+                    maquinaCafe.setVta_acum_cafexpress(totalVenta);
+                    maquinaCafe.setCant_acum_cafexpress(cantidaCafe);
+                    break;
+                }
+                case 2: {
+                    // Se muestra el total de la venta
+                    JOptionPane.showMessageDialog(null, "Total Venta ($)");
+                    maquinaCafe.mostrarVentaAcum();
+                    break;
+                }
+                case 3: {
+                    // Se muestra la cantidad de cafe
+                    JOptionPane.showMessageDialog(null, "Cantidad de Cafe(vta)");
+                    maquinaCafe.mostrarCantAcum();
+                    break;
+                }
+                case 4: {
+                    // Se sale de la maquina de cafe
+                    JOptionPane.showMessageDialog(null, "Saliendo de la maquina de cafe");
+                    System.exit(0);
+                    break;
+                }
+                default:
+                    // Se muestra un mensaje de error si la opcion no es valida
+                    JOptionPane.showMessageDialog(null, "Opcion no valida");
+            }
+        }while (true);// Se repite el ciclo hasta que se elija la opcion 4
+    }
+}
---
+++ Ejercicio6/Maquina_Cafe/src/main/java/org/example/MaquinaCafe.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import javax.swing.*;
+
+// Clase MaquinaCafe que contiene los metodos para mostrar la venta acumulada y la cantidad acumulada
+public class MaquinaCafe {
+
+    // Atributos de la clase MaquinaCafe
+    private int precio_cafe_express;
+    private int vta_acum_cafexpress;
+    private int cant_acum_cafexpress;
+
+    // Constructor de la clase MaquinaCafe
+    public MaquinaCafe(int precio_cafe_express, int vta_acum_cafexpress, int cant_acum_cafexpress) {
+        this.precio_cafe_express = precio_cafe_express;
+        this.vta_acum_cafexpress = vta_acum_cafexpress;
+        this.cant_acum_cafexpress = cant_acum_cafexpress;
+    }
+
+    // Metodos de la clase MaquinaCafe para mostrar la venta acumulada y la cantidad acumulada
+    public int getPrecio_cafe_express() {
+        return precio_cafe_express;
+    }
+
+    public void setVta_acum_cafexpress(int vta_acum_cafexpress) {
+        this.vta_acum_cafexpress = vta_acum_cafexpress;
+    }
+
+    public void setCant_acum_cafexpress(int cant_acum_cafexpress) {
+        this.cant_acum_cafexpress += cant_acum_cafexpress;
+    }
+
+    // Metodos de la clase MaquinaCafe para mostrar la venta acumulada y la cantidad acumulada
+    public void  mostrarVentaAcum(){
+        JOptionPane.showMessageDialog(null, "Venta acumulada: " + this.vta_acum_cafexpress);
+    }
+
+    public void mostrarCantAcum(){
+        JOptionPane.showMessageDialog(null, "Cantidad acumulada: " + this.cant_acum_cafexpress);
+    }
+}
