33f46390f0d61488fa981e811231c0fd042940bf
Pablo Burgos <111798287+DevCodeExplorer-7@users.noreply.github.com>
2023-11-03 19:13:40 -0300

    Ejercicio Tarjeta_Bip

---
+++ Ejercicio7/Tarjeta_Bip/src/main/java/org/example/Totem.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import javax.swing.*;
+
+// Clase Totem
+public class Totem {
+    public static void main(String[] args) {
+        // Instancia de la clase Bip
+        Bip bip = new Bip("123456789", 0);
+
+        // Menu de opciones
+        String menu = """
+                ***** Menu Totem *****
+                1.- Cargar Saldo de la Tarjeta
+                2.- Descontar Saldo de la Tarjeta
+                3.- Ver Datos de la Tarjeta
+                4.- Salir
+                """;
+        // Variables
+        int opcion, monto;
+        do {
+            // Menu de opciones
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+            // Switch de opciones
+            switch (opcion) {
+                case 1 : {
+                    // Cargar saldo
+                    JOptionPane.showMessageDialog(null, "Cargando Saldo ");
+                    monto = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el monto a cargar"));
+                    bip.cargarSaldo(monto);
+                    break;
+                }
+                case 2 :{
+                    // saldo descontado
+                    JOptionPane.showMessageDialog(null, "Saldo Descontado");
+                    bip.pagar();
+                    break;
+                }
+                case 3: {
+                    // Mostrar datos
+                    JOptionPane.showMessageDialog(null, "Datos de la tarjeta");
+                    bip.mostrarDatos();
+                    break;
+                }
+                case 4: {
+                    // Salir
+                    JOptionPane.showMessageDialog(null, "Gracias por usar el totem");
+                    System.exit(0);
+                    break;
+                }
+                // Opcion no valida
+                default: JOptionPane.showMessageDialog(null, "Opción no válida: " + opcion);
+            }
+        }while (true);
+    }
+}
